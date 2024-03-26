215397fe430190a7e8b64b9b701d264faa8ffe21
Muzafar Ali <[valid-email]>
2024-03-26 10:43:01 +0500

    add two more operation **/&

--- index.ts
+++ index.ts
@@ -1,23 +1,45 @@
 import inquirer from "inquirer"
+
 const answer = await inquirer.prompt([
-    {message:"Enter first Number",type:"number",name:"firstNumber"},
-    {message:"Enter Second Number",type:"number",name:"SecondNumber"},
-    {message:"select one of the operators perform operation",type:"list",name:"operator",
-choices:["Addition","Subtraction", "Multiplaction", "Division"]},
+    {
+        message:"Enter first Number",
+        type:"number",
+        name:"firstNumber"
+     },
+    {
+        message:"Enter Second Number",
+        type:"number",
+        name:"SecondNumber"
+    },
+    {
+        message:"select one of the operators perform operation",
+        type:"list",
+        name:"operator",
+       choices:
+       ["+","-","*", "/","**","%"]
+    },
 ]);
-if(answer.operator === "Addition"){
+
+if(answer.operator === "+"){
           console.log(answer.firstNumber + answer.SecondNumber);
           
-}else if(answer.operator === "Subtraction"){
+}else if(answer.operator === "-"){
     console.log(answer.firstNumber - answer.SecondNumber);
 
-}else if(answer.operator === "Multiplaction"){
+}else if(answer.operator === "*"){
     console.log(answer.firstNumber * answer.SecondNumber);
 
-}else if(answer.operator === "Division"){
+}else if(answer.operator === "/"){
     console.log(answer.firstNumber / answer.SecondNumber);
+
+}else if(answer.operator === "**"){
+    console.log(answer.firstNumber ** answer.SecondNumber);
+
+}else if(answer.operator === "%"){
+    console.log(answer.firstNumber % answer.SecondNumber);
 
 }else{
     console.log("pleace select valid operator");
-    
 }
+
+//this is my fist project 
